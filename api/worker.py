877d3f911c3e38fa2476b6f7c73535fd3f011d2a
Main <main@Mains-MBP.hitronhub.home>
2022-08-17 11:41:33 -0400

    fixing function

---
+++ api/worker.py
@@ -0,0 +1,16 @@
+import os
+
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDIS_URL', 'redis-19572.c281.us-east-1-2.ec2.cloud.redislabs.com:19572')
+os.system("python -m tkinter")
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
