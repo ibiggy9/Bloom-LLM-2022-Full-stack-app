1478178831c21570e03aca2347c3fffde3284045
Main <main@Mains-MBP.hitronhub.home>
2022-08-16 22:52:25 -0400

    fixing function

--- api/hf.py
+++ api/hf.py
@@ -71,7 +71,8 @@ def infer(prompt,
     proc_time = time.time()-s
     #print(f"Processing time was {proc_time} seconds")
     text = list(response[0].items())[0] 
-    return text[1]
+    finalText = text[1].strip()
+    return finalText
 
 
 
